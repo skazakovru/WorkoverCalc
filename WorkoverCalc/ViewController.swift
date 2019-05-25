db41d6de4fbb8cb33b48a698146b1370e9fbdee8
Sergei Kazakov <skazakovru@gmail.com>
2019-05-25 12:48:46 +0300

    Initial Commit

---
+++ WorkoverCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkoverCalc
+//
+//  Created by Sergei Kazakov on 5/25/19.
+//  Copyright © 2019 Sergei Kazakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
