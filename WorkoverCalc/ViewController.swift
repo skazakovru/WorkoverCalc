689e21f7fb33b52e95b156669d9f645e29993fb7
Sergei Kazakov <skazakovru@gmail.com>
2019-05-26 13:05:02 +0300

    Decimal Pad added to the first View text fields.

--- WorkoverCalc/ViewController.swift
+++ WorkoverCalc/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var names = [String]()
+    var segueIdentities = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        names = ["Tubing/casing communication TVD", "Second", "Third", "Fourth", "Sixth", "Seventh", "Eighth", "Nineth", "Tenth"]
+        segueIdentities = ["A","B","C","D","E","F","G","H","I","J"]
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as UITableViewCell
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let vcName = segueIdentities[indexPath.row]
+        let viewController = (storyboard?.instantiateViewController(withIdentifier: vcName))
+        self.navigationController?.pushViewController(viewController!, animated: true)
+    }
 }
 
